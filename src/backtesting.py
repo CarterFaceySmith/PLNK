d3e6639cbda3af436ea6ce14b615cbbb9c795238
CarterFaceySmith <carterfaceysmith@gmail.com>
2023-02-08 10:10:11 +1100

    refactoring to intake tickers and splits from cli

--- src/backtesting.py
+++ src/backtesting.py
@@ -10,14 +10,12 @@ tickers = input("Enter the stock tickers separated by commas: ").split(',')
 allocation_input = input("Enter the target allocations for each stock separated by commas (e.g. 0.3,0.2,0.5): ").split(',')
 target_allocations = [float(x) for x in allocation_input]
 
-# Ensure that the sum of target allocations is 1
 if sum(target_allocations) > 1 or sum(target_allocations) < 0:
     raise ValueError("The sum of your allocations must be between 0 and 1")
 
-portfolio = dict(zip(tickers, target_allocations))
+weights = dict(zip(tickers, target_allocations))
 
 def backtest(strategy, strat_params, symbols, start, end, timeframe=TimeFrame.Day, cash=10000):
-    # initialize backtrader broker
     cerebro = bt.Cerebro(stdstats=True)
     cerebro.broker.setcash(cash)
     cerebro.addstrategy(strategy, strat_params)
@@ -40,7 +38,6 @@ def backtest(strategy, strat_params, symbols, start, end, timeframe=TimeFrame.Da
             data = bt.feeds.PandasData(dataname=alpaca_data, name=symbol)
             cerebro.adddata(data)
 
-    # Run testing
     initial_portfolio_value = cerebro.broker.getvalue()
     print(f'Starting Portfolio Value: {initial_portfolio_value}')
     results = cerebro.run()
@@ -49,20 +46,20 @@ def backtest(strategy, strat_params, symbols, start, end, timeframe=TimeFrame.Da
 
     strat = results[0]
     print('Sharpe Ratio:', strat.analyzers.mysharpe.get_analysis()['sharperatio'])
-    # cerebro.plot(iplot= False)
+    # cerebro.plot()
 
 class Rebalance(bt.Strategy):
    
    params = (
-       ('weights',{})
+       ('weights',{}),
    )
 
    def __init__(self, params=None):
-       if params != None:
-            for name, val in params.items():
-                setattr(self.params, name, val) # Could change to directly write as per below line 65?
+    #    if params != None:
+    #         for name, val in params.items():
+    #             setattr(self.params, name, val) # Could change to directly write as per below line 65?
        self.year_last_rebalanced = -1 
-    #    self.weights = { "BTCUSD" : 0.15, "MSFT" : 0.2, "ETHUSD" : 0.05, "VOO" : 0.4, "VOOG" : 0.2}
+       self.weights = params
 
    def next(self):
        # if we’ve already rebalanced this year
@@ -74,6 +71,6 @@ class Rebalance(bt.Strategy):
        for i,d in enumerate(self.datas):
            # rebalance portfolio with desired target percents
             symbol = d._name
-            self.order_target_percent(d, target=self.weights[symbol])
+            self.order_target_percent(d, target=self.params.weights[symbol])
 
-backtest(Rebalance, portfolio, tickers, '2022-01-01', '2023-02-01', TimeFrame.Day, 100000)
+backtest(Rebalance, weights, tickers, '2022-01-01', '2023-02-01', TimeFrame.Day, 100000)
