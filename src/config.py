8076d5c72e44450132cebcdd6dce4f43470df6d6
CarterFaceySmith <carterfaceysmith@gmail.com>
2023-02-08 12:26:55 +1100

    rebalance script refactored to be dynamic

--- src/config.py
+++ src/config.py
@@ -1,2 +1,18 @@
-API_KEY = 'PKP2JJKBLLVB8WN2BDCR'
-SECRET_KEY = '4zAN8B8CUpjKLfy7uUtmOgcthTPo5CMl1a3fn2Ym'
+from alpaca_trade_api.rest import REST
+from alpaca.trading.client import TradingClient
+from alpaca.data import CryptoHistoricalDataClient, StockHistoricalDataClient
+
+# Settings
+API_KEY = 'PKB0OUCULGGX08VG0KW0'
+SECRET_KEY = 'EpsYl1t06L03vecfmzK0aZNgcQ4JHDDWHdiSSgQU'
+crypto = ['BTCUSD', 'ETHUSD']
+
+# Clients
+trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
+rest_api = REST(API_KEY, SECRET_KEY, 'https://paper-api.alpaca.markets')
+crypto_client = CryptoHistoricalDataClient()
+stock_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
+
+#Functions
+def return_clients():
+    return trading_client,rest_api,crypto_client,stock_client
