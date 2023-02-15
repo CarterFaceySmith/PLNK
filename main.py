6ad9bac55d293922e1678fbbbea8ae374905be43
CarterFaceySmith <carterfaceysmith@gmail.com>
2023-02-15 13:05:49 +1100

    huge refactoring, menus added, debugging, etc

--- main.py
+++ main.py
@@ -2,31 +2,32 @@
 
 def main():
     print("Welcome back, Sir.")
-    mode = input("Select your trading mode:\n1. Backtest\n2. Rebalance\n3. Liquidate portfolio\n4. Check stats\n5. Add ticker\n6. Switch portfolio preset\n7. Exit\n> ")
+    mode = input("Select your trading mode:\n1. Backtest/Optimise\n2. Rebalance\n3. Liquidate portfolio\n4. Check stats\n5. Add ticker\n6. Switch portfolio preset\n7. Exit\n> ")
     match mode:
         case "1":
-            #Backtest
+            # Prompt for mode selection
+            # Backtest/Optimise, pass in mode 'BACKTEST' or 'OPTIMISE'
             pass
         case '2':
-            #Rebalance
+            # Rebalance, pass in mode '
             pass
         case '3':
-            #Liquidate
+            # Liquidate
             pass
         case '4':
-            #Check stats
+            # Check stats
             pass
         case '5':
-            #Add ticker
+            # Add ticker
             pass
         case '6':
-            #Switch portfolio preset
+            # Switch portfolio preset
             pass
         case '7':
-            #Default exit
+            # Default exit
             exit(0)
         case _:
-            #Error catch exit
+            # Error catch exit
             exit(1)
 
 if __name__ == '__main__':
