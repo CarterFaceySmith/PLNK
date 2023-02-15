e47197d1cb086ed1e47f2d097baec37407f7c8d1
CarterFaceySmith <carterfaceysmith@gmail.com>
2023-02-15 17:14:46 +1100

    fkn nonsense w optimise(), added menus

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
-# import src.rebalance as rebalance, src.backtest as bktest, src.config as config
+from src import config
 
 def main():
-    print("Welcome back, Sir.")
-    mode = input("Select your trading mode:\n1. Backtest/Optimise\n2. Rebalance\n3. Liquidate portfolio\n4. Check stats\n5. Add ticker\n6. Switch portfolio preset\n7. Exit\n> ")
+    print("Welcome.")
+    mode = input("Select your trading mode:\n\t1. Backtest/Optimise\n\t2. Rebalance\n\t3. Liquidate portfolio\n\t4. Check stats\n\t5. Exit\n")
+
     match mode:
         case "1":
-            # Prompt for mode selection
-            # Backtest/Optimise, pass in mode 'BACKTEST' or 'OPTIMISE'
-            pass
+            bt_opt_mode = input("Select your mode:\t\n1. Backtest\t\n2. Optimise\n")
+            match bt_opt_mode:
+                case "1":
+                    backtesting.bt_opt_init(mode='BACKTEST')
+                case "2":
+                    backtesting.bt_opt_init(mode='OPTIMISE')
+                case _:
+                    raise ValueError("Invalid input")
+                
         case '2':
-            # Rebalance, pass in mode '
-            pass
+            tickers = input("Enter the desired portfolio ticker(s) separated by commas: ").split(',')
+            allocation_input = input("Enter the target allocations for each ticker separated by commas (e.g. 0.3,0.2,0.5): ").split(',')
+            target_allocations = [float(x) for x in allocation_input]
+
+            if sum(target_allocations) > 1 or sum(target_allocations) < 0:
+                raise ValueError("The sum of your allocations must be between 0 and 1")
+
+            for ticker in tickers:
+                if len(ticker) > 6:
+                    raise ValueError(f"Tickers must be 6 characters or less, {ticker} is invalid")
+                
+            portfolio = dict(zip(tickers, target_allocations))
+            prec = input("Enter the decimal precision of the rebalance (defaults to 3 if none entered): ")
+
+            rebalance.rebalance(portfolio, prec)
+
         case '3':
-            # Liquidate
-            pass
+            confirm = input("Confirm liquidation of portfolio positions? (y/n)")
+            if confirm == 'y':
+                config.liquidate_portfolio()
+
         case '4':
-            # Check stats
-            pass
+            config.get_portfolio_stats()
+
         case '5':
-            # Add ticker
-            pass
-        case '6':
-            # Switch portfolio preset
-            pass
-        case '7':
             # Default exit
+            print("Goodbye.")
             exit(0)
+
         case _:
             # Error catch exit
-            exit(1)
-
+            raise BaseException("Invalid input or crash")
+        
 if __name__ == '__main__':
     main()
