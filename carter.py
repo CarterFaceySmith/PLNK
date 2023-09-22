ec180e610fb238a7d973a8544131a88e729217f4
CarterFaceySmith <carterfaceysmith@gmail.com>
2023-09-22 12:44:37 +0200

    x

--- carter.py
+++ carter.py
@@ -5,9 +5,8 @@ from alpaca.trading.client import TradingClient
 portfolio = {'VOO': 0.4, 'VOOG': 0.1, 'IBM': 0.05, 'BTC-USD': 0.2, 'ETH-USD': 0.2}
 
 async def main():
-    client = TradingClient(config.API_KEY, config.SECRET_KEY, paper=False)
     await config.send_message("---------------------\nRebalancing:\n")
-    rebalance.perform_live_rebalance(t_client=client, desired_allocations=portfolio)
+    rebalance.perform_live_rebalance(t_client=config.live_client, desired_allocations=portfolio)
     await config.send_update_msg()
     await config.send_message("\n---------------------\nRebalancing complete.")
     exit()
