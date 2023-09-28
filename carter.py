0fcb3a4cde2114553cd4ae5d1739093bfef6cac6
CarterFaceySmith <carterfaceysmith@gmail.com>
2023-09-28 13:38:44 +0200

    Created daemon, added several small changes

--- carter.py
+++ carter.py
@@ -3,12 +3,13 @@ from src import config, rebalance, backtesting, testing
 from alpaca.trading.client import TradingClient
 
 # portfolio = {'VOO': 0.4, 'VOOG': 0.1, 'IBM': 0.05, 'BTCUSD': 0.2, 'ETHUSD': 0.2}
-portfolio = {'VOO': 0.33, 'VOOG': 0.1, 'IBM': 0.05, 'BTC-USD': 0.33, 'ETH-USD': 0.17,}
+portfolio = {'VOO': 0.33, 'VOOG': 0.1, 'IBM': 0.05, 'BTCUSD': 0.33, 'ETHUSD': 0.17,}
 
 
 async def main():
-    input("Press Enter to confirm live rebalance...")
+    # input("Press Enter to confirm live rebalance...")
     await rebalance.perform_live_rebalance(trading_client=config.live_client, desired_allocations=portfolio)
+    # await config.send_message("Test message - scheduled job performed by daemon.")
     exit()
 
 if __name__ == '__main__':
