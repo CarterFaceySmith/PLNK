6101244a4f9988fd63c736786e9bdf34bfadfe50
CarterFaceySmith <carterfaceysmith@gmail.com>
2023-11-25 21:02:17 +0100

    Merged.

---
+++ client/src/expanded_theme.ts
@@ -0,0 +1,12 @@
+// eslint-diasble-next-line
+import { Palette, PaletteColor } from "@mui/material/styles/createPalette",
+
+declare module "@mui/material/styles/createPalette" {
+  interface PaletteColor {
+    [key: number: string;
+  }
+
+  interface Palette {
+    tertiary: PaletteColor;
+  }
+}
